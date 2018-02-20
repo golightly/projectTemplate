f849fa24a394205960fc9f0789c99373497132d3
trashviolent <wizzardnoise@gmail.com>
2018-02-20 14:04:47 -0500

    Update scene.cpp

--- engine/scene.cpp
+++ engine/scene.cpp
@@ -26,7 +26,43 @@ void setupScene(Program &program) {
         program.scene.spriteNum += atoi(program.input.c_str());
     }
     setupSprite(program);
-    //continue here
+    std:getline(program.readFile, program.input);
+    program.scene.collisionNum = atoi(program.input.c_str());
+    program.scene.collisionAttributes = new SDL_Rect[program.scene.collisionNum];
+    for(int a = 0; a < program.scene.collisionNum; ++a) {
+        std::getline(program.readFile, program.input);
+        program.scene.collisionAttributes[a].w = atoi(program.input.c_str());
+        std::getline(program.readFile, program.input);
+        program.scene.collisionAttributes[a].h = atoi(program.input.c_str());
+        std::getline(program.readFile, program.input);
+        program.scene.collisionAttributes[a].x = atoi(program.input.c_str());
+        std::getline(program.readFile, program.input);
+        program.scene.collisionAttributes[a].y = atoi(program.input.c_str());
+    }
+    std::getline(program.readFile, program.input);
+    program.scene.spawnGroupNum = atoi(program.input.c_str());
+    program.scene.spawnGroupSize = new int[program.scene.spawnGroupNum];
+    for(int a = 0; a < program.scene.spawnGroupNum; ++a) {
+        std::getline(program.readFile, program.input);
+        program.scene.spawnGroupSize[a] = atoi(program.input.c_str());
+    }
+    program.scene.spawnArea = new SDL_Rect*[program.scene.spawnGroupNum];
+    for(int a = 0; a < program.scene.spawnGroupNum; ++a) {
+        program.scene.SpawnArea[a] = new SDL_Rect[program.scene.spawnGroupSize[a]];
+    }
+    for(int a = 0; a < program.scene.spawnGroupNum; ++a) {
+        for(int b = 0; b < program.scene.spawnGroupSize[a]; ++b) {
+            std::getline(program.readFile, program.input);
+            program.scene.spawnArea[a][b].w = atoi(program.input.c_str());
+            std::getline(program.readFile, program.input);
+            program.scene.spawnArea[a][b].h = atoi(program.input.c_str());
+            std::getline(program.readFile, program.input);
+            program.scene.spawnArea[a][b].x = atoi(program.input.c_str());
+            std::getline(program.readFile, program.input);
+            program.scene.spawnArea[a][b].y = atoi(program.input.c_str());
+        }
+    }
+    //will require a lot more once characters and stuff added
 }
 
 //work on save next
