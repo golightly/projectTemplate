5bbfa36d0a7941c26703f36cad14b26421e810a7
trashviolent <wizzardnoise@gmail.com>
2018-02-20 15:40:37 -0500

    Update openScene.cpp

--- engine/openScene.cpp
+++ engine/openScene.cpp
@@ -1,1 +1,10 @@
 #include "openScene.h"
+#include "program.h"
+#include "scene.h"
+#include "editor.h"
+
+void openScene(Program &program) {
+  //scene already loaded, so it doesn't need to know which scene is loaded, except for saving purposes
+  //use editor to store all the sdl and editing scene related stuff
+  //have editor also store the path to save to, for simplicity
+}
