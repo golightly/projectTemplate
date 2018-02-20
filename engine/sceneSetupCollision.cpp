556a53c9a62bd5f31c9f7c22ddcbc303c5baf4d4
trashviolent <wizzardnoise@gmail.com>
2018-02-20 15:14:15 -0500

    Update sceneSetupCollision.cpp

--- engine/sceneSetupCollision.cpp
+++ engine/sceneSetupCollision.cpp
@@ -1,1 +1,24 @@
+#include "sceneSetupCollision.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <SDL.h>
+#include "program.h"
+#include "scene.h"
 
+void sceneSetupCollision(Program &program) {
+  std:getline(program.readFile, program.input);
+  program.scene.collisionNum = atoi(program.input.c_str());
+  program.scene.collisionAttributes = new SDL_Rect[program.scene.collisionNum];
+  for(int a = 0; a < program.scene.collisionNum; ++a) {
+    std::getline(program.readFile, program.input);
+    program.scene.collisionAttributes[a].w = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.collisionAttributes[a].h = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.collisionAttributes[a].x = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.collisionAttributes[a].y = atoi(program.input.c_str());
+  }
+}
