de0b440a3922a97a8d2c35b8be791c6ec180287d
trashviolent <wizzardnoise@gmail.com>
2018-02-20 13:36:42 -0500

    Update scene.cpp

--- engine/scene.cpp
+++ engine/scene.cpp
@@ -3,61 +3,30 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <stdlib.h>
-#include "image.h"
+#include <cstdlib>
+#include "program.h"
+#include "sprite.h"
 
-void setupScene(std::ifsteam &file, Scene &scene, std::string &input, Image* image, std::string &scenePath) {
-    std::getline(file, scene.sceneName);
-    std::getline(file, scenePath);
-    std::getline(file, scene.functionName);
-    std::getline(file, input);
-    scene.sceneAttributes.w = atoi(input.c_str());
-    std::getline(file, input);
-    scene.sceneAttributes.h = atoi(input.c_str());
-    scene.sceneAttributes.x = 0; scene.sceneAttributes.y = 0;
-    std::getline(file, input);
-    scene.sceneImageNum = atoi(input.c_str());
-    scene.image = new std::string[scene.sceneImageNum];
-    for(int a = 0; a < scene.sceneImageNum; ++a) {
-        std::getline(file, input);
-        scene.image[a] = atoi(input.c_str());
+void setupScene(Program &program) {
+    program.scene = new Scene;
+    program.scene.sceneName = program.sceneName[atoi(program.sceneImagePath.c_str())];
+    program.readFile.open(program.scenePath[atoi(program.sceneImagePath.c_str())].c_str());
+    std::getline(program.readFile, program.scene.functionName);
+    std::getline(program.readFile, program.input);
+    program.scene.sceneAttributes.w = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.sceneAttributes.h = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.sceneImageNum = atoi(program.input.c_str());
+    program.scene.image = new int[program.scene.sceneImageNum];
+    program.scene.spriteNum = 0;
+    for(int a = 0; a < program.scene.sceneImageNum; ++a) {
+        std::getline(program.readFile, program.input);
+        program.scene.image[a] = atoi(program.input.c_str());
+        program.scene.spriteNum += atoi(program.input.c_str());
     }
-    scene.spritePerTexture = new int[scene.sceneImageNum];
-    scene.spriteAttributes = new SDL_Rect*[scene.sceneImageNum];
-    for(int a = 0; a < scene.sceneImageNum; ++a) {
-        std::getline(file, input);
-        scene.spritePerTexture[a] = atoi(input.c_str());
-        scene.spriteAttributes[a] = new SDL_Rect[scene.spritePerTexture[a]];
-    }
-    for(int a = 0; a < scene.sceneImageNum; ++a) {
-        for(int b = 0; b < scene.spritePerTexture[a]; ++a) {
-            scene.spriteAttributes[a][b].w = image[a].w;
-            scene.spriteAttributes[a][b].h = image[a].h;
-            std::getline(file, input);
-            scene.spriteAttributes[a][b].x = atoi(input.c_str());
-            std::getline(file, input);
-            scene.spriteAttributes[a][b].y = atoi(input.c_str());
-        }
-    }
-    std::getline(file, input);
-    scene.collisionNum = atoi(input.c_str());
-    scene.collisionAttributes = new SDL_Rect[scene.collisionNum];
-    for(int a = 0; a < scene.collisionNum: ++a) {
-        std::getline(file, input);
-        scene.collisionAttributes[a].w = atoi(input.c_str());
-        std::getline(file, input);
-        scene.collisionAttributes[a].h = atoi(input.c_str());
-        std::getline(file, input);
-        scene.collisionAttributes[a].x = atoi(input.c_str());
-        std::getline(file, input);
-        scene.collisionAttributes[a].y = atoi(input.c_str());
-    }
-    std::getline(file, input);
-    scene.spawnGroupNum = atoi(input.c_str());
-    std::getline(file, input);
-    scene.spawnGroupSize = atoi(input.c_str());
-    scene.spawnArea = new SDL_Rect*[scene.spawnGroupNum];
-    for(int a = 0; a < scene.spawnGroupNum; ++a) {
-        scene.spawnArea[a] = new SDL_Rect[scene.spawnGroupSize];
-    }
+    setupSprite(program);
+    //continue here
 }
+
+//work on save next
