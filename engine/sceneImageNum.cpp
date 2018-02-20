03d6b8e8e3e884dc669026d7ba259c4978189f58
trashviolent <wizzardnoise@gmail.com>
2018-02-20 14:58:28 -0500

    Update sceneImageNum.cpp

--- engine/sceneImageNum.cpp
+++ engine/sceneImageNum.cpp
@@ -1,1 +1,21 @@
 #include "sceneImageNum.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "scene.h"
+#include "program.h"
+#include "sprite.h"
+
+void sceneImageNum(Program &program) {
+  std::getline(program.readFile, program.input);
+  program.scene.sceneImageNum = atoi(program.input.c_str());
+  program.scene.image = new int[program.scene.sceneImageNum];
+  program.scene.spriteNum = 0;
+  for(int a = 0; a < program.scene.sceneImageNum; ++a) {
+    std::getline(program.readFile, program.input);
+    program.scene.image[a] = atoi(program.input.c_str());
+    program.scene.spriteNum += atoi(program.input.c_str());
+  }
+  setupSprite(program);
+}
