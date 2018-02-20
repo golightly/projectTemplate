51c13dcedc628c6ef1984fa4d9c75008e10a47af
trashviolent <wizzardnoise@gmail.com>
2018-02-20 17:41:53 -0500

    Update overhead.h

--- engine/overhead.h
+++ engine/overhead.h
@@ -1,14 +1,12 @@
 #pragma once
 
 #include <SDL.h>
-#include <SDL_image.h>
 
 struct Overhead {
     SDL_Window* window;
-    int imgLoad, w, h;
     SDL_Renderer* renderer;
 };
 
 void setupOverhead(Overhead &overhead, int w, int h);
 
-void close(Overhead &overhead, Texture &texture);
+void closeOverhead(Overhead &overhead);
