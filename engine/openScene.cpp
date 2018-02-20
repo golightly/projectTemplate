85442fa4248319a1ef44ee0470e6b9e3e89e116a
trashviolent <wizzardnoise@gmail.com>
2018-02-20 16:10:53 -0500

    Update openScene.cpp

--- engine/openScene.cpp
+++ engine/openScene.cpp
@@ -7,4 +7,12 @@ void openScene(Program &program) {
   //scene already loaded, so it doesn't need to know which scene is loaded, except for saving purposes
   //use editor to store all the sdl and editing scene related stuff
   //have editor also store the path to save to, for simplicity
+  Editor editor;
+  std::cout << "width of editor window: ";
+  std::cin >> editor.windowWidth;
+  std::cout << "height of editor window: ";
+  std::cin >> editor.windowHeight;
+  editor.savePath = program.scenePath[atoi(program.sceneImagePath.c_str())];
+  setupEditor(editor, program.scene);
+  runEditor(editor, program.scene);
 }
