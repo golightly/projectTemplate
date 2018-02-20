274284383909ae1e7a410282d1de2ca2b5b9d471
trashviolent <wizzardnoise@gmail.com>
2018-02-20 12:46:12 -0500

    Update removeScene.cpp

--- engine/removeScene.cpp
+++ engine/removeScene.cpp
@@ -1,7 +1,33 @@
 #include "removeScene.h"
 #include <fstream>
+#include <iostream>
+#inclue <string>
+#include <cstdlib>
+#include <cstdio>
 #include "program.h"
 #include "scene.h"
 
-//continue here, and also work on removeImage.cpp.  be sure to actually delete the scene from both the
-//hard drive and from the program file
+void removeScene(Program &program) {
+  std::cout << "delete scene from hard drive(y/n): ";
+  std::cin >> program.input;
+  if(program.input == "y")
+    remove(program.scenePath[program.sceneImagePath.c_str()].c_str());
+  std::string* temp = new std::string[program.sceneNum - 1];
+  std::string* temp2 = new std::string[program.sceneNum - 1];
+  int b = 0;
+  for(int a = 0; a < program.sceneNum; ++a) {
+    if(a != atoi(program.sceneImagePath.c_str())) {
+      temp[b] = program.scenePath[a];
+      temp2[b] = program.sceneName[a];
+      ++b;
+    }
+  }
+  delete [] program.scenePath;
+  delete [] program.sceneName;
+  --program.sceneNum;
+  for(int a = 0; a < program.sceneNum; ++a) {
+    program.scenePath[a] = temp[a];
+    program.sceneName = temp2[a];
+  }
+
+//work on setupScene update next
