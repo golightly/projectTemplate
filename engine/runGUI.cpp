e9fc4cc2e2347a7cc730db9f4ef993e5d193b1e0
trashviolent <wizzardnoise@gmail.com>
2018-02-20 18:08:40 -0500

    Update runGUI.cpp

--- engine/runGUI.cpp
+++ engine/runGUI.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include "editor.h"
 #include "scene.h"
+#include "overhead.h"
 
 void runGUI(std::string windowName, Editor &editor, Scene &scene) {
-  
+  setupOverhead(editor.GUIOverhead, editor.windowWidth, editor.windowHeight, windowName);
+  //continue here
 }
