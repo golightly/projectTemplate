50465d7855ff5d424ca20ee3cd9635ff4c25bfd4
trashviolent <wizzardnoise@gmail.com>
2018-02-20 13:25:35 -0500

    Update sprite.h

--- engine/sprite.h
+++ engine/sprite.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <SDL.h>
+#include <string>
 
 struct Sprite {
+    std::string spriteName;
     int textureIndex;
     SDL_Rect attributes;
 };
 
 void setupSprite(Sprite &sprite, int w, int h, int x, int y, int textureIndex);
+
+void setupSprite(Program &program);
