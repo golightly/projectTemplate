c4f1c72661f477d4e2433b2eaad7911f0ecf4f42
trashviolent <wizzardnoise@gmail.com>
2018-02-20 15:25:05 -0500

    Update resetSDKData.cpp

--- engine/resetSDKData.cpp
+++ engine/resetSDKData.cpp
@@ -14,6 +14,7 @@ void resetSDKData(Program &program) {
         delete [] sprite;
         delete [] program.scene.image;
         delete program.scene;
+        program.scene = NULL;
     }
     program.input.clear();
     program.commandType.clear();
