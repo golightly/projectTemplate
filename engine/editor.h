5532ecac66cb9f4334089ef30ee3ece8dfaad512
trashviolent <wizzardnoise@gmail.com>
2018-02-20 16:19:17 -0500

    Update editor.h

--- engine/editor.h
+++ engine/editor.h
@@ -1,6 +1,19 @@
 #pragma once
 
+#include <string>
+#include "overhead.h"
+#include "scene.h"
+
 //put all the sdl and scene editing stuff here
 struct Editor {
-  
+  Overhead overhead;
+  int windowWidth, windowHeight;
+  std::string savePath;
+  SDL_Event event;
+  bool quitEditor;
+  Texture* texture;
+  //also have buttons and stuff for the editor only here
+  //plan out how editing will actually work next
 };
+
+void setupEditor(Editor &editor, Scene &scene);
