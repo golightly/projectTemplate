1cb635e3d47e5be3aaa841156936ae085016d9f0
trashviolent <wizzardnoise@gmail.com>
2018-02-21 18:50:11 -0500

    Update runGUI.cpp

--- engine/runGUI.cpp
+++ engine/runGUI.cpp
@@ -3,9 +3,22 @@
 #include "editor.h"
 #include "scene.h"
 #include "overhead.h"
+#include "texture.h"
+#include "image.h"
 
-void runGUI(std::string windowName, Editor &editor, Scene &scene) {
+void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) {
   setupOverhead(editor.GUIOverhead, editor.windowWidth, editor.windowHeight, windowName);
-  
+  editor.texture.texture = NULL;
+  if(scene.sceneImageNum > 0) {
+    int* w = new int[scene.sceneImageNum];
+    int* h = new int[scene.sceneImageNum];
+    std::string* path = new std::string[scene.sceneImageNum];
+    for(int a = 0; a < scene.sceneImageNum; ++a) {
+      w[a] = (int)image[scene.image[a]].w;
+      h[a] = (int)image[scene.image[a]].h;
+      path[a] = image[scene.image[a]].path;
+    }
+    setupTexture(editor.GUIOverhead, editor.texture, w, h, path, scene.sceneImageNum);
+  }
   //continue here
 }
