11fa48be8c5a2788fafe0804c59206c37c243812
trashviolent <wizzardnoise@gmail.com>
2018-02-21 19:09:03 -0500

    Update overhead.cpp

--- engine/overhead.cpp
+++ engine/overhead.cpp
@@ -6,13 +6,13 @@
 #include "texture.h"
 
 void setupOverhead(Overhead &overhead, int w, int h, std::string windowName, Editor &editor) {
-    editor.mu.lock();
+    editor.sdl_lock.lock();
     if(SDL_WasInit(SDL_INIT_VIDEO) == 0) {
         SDL_Init(SDL_INIT_VIDEO);
         overhead.imgFlags = IMG_INIT_PNG;
         IMG_INIT(overhead.imgFlags;
     }
-    editor.mu.unlock();
+    editor.sdl_lock.unlock();
     overhead.window = SDL_CreateWindow(windowName.c_str(), SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     overhead.renderer = SDL_CreateRenderer(overhead.window, -1, SDL_RENDERER_ACCELERATED);
     SDL_SetRenderDrawColor(overhead.renderer, 0, 0, 0, 0);
