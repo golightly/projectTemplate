3b32946f66a883a2f08f1a178fb7fd082fda5f3f
trashviolent <wizzardnoise@gmail.com>
2018-02-21 20:41:47 -0500

    Update sceneHeightWidth.cpp

--- engine/sceneHeightWidth.cpp
+++ engine/sceneHeightWidth.cpp
@@ -1,1 +1,14 @@
 #include "sceneHeightWidth.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "scene.h"
+#include "program.h"
+
+void sceneHeightWidth(Program &program) {
+  std::getline(program.readFile, program.input);
+  program.scene.width = atoi(program.input.c_str());
+  std::getline(program.readFile, program.input);
+  program.scene.height = atoi(program.input.c_str());
+}
