b72d5763c742a88395629bc0d8904bcb3a2d7439
trashviolent <wizzardnoise@gmail.com>
2018-02-21 19:20:33 -0500

    Update runGUI.cpp

--- engine/runGUI.cpp
+++ engine/runGUI.cpp
@@ -5,10 +5,13 @@
 #include "overhead.h"
 #include "texture.h"
 #include "image.h"
+#include "access.h"
 
 void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) {
+  accessData();
   setupOverhead(editor.GUIOverhead, editor.windowWidth, editor.windowHeight, windowName);
   editor.texture.texture = NULL;
+  editor.editorTexture = NULL;
   if(scene.sceneImageNum > 0) {
     int* w = new int[scene.sceneImageNum];
     int* h = new int[scene.sceneImageNum];
@@ -20,5 +23,18 @@ void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) 
     }
     setupTexture(editor.GUIOverhead, editor.texture, w, h, path, scene.sceneImageNum);
   }
+  delete [] w;
+  delete [] h;
+  delete [] path;
+  w = new int;
+  h = new int;
+  path = new std::string;
+  w = 50;
+  h = 50;
+  path = "collisionBox.png";
+  setupTexture(editor.GUIOverhead, editor.editorTexture, path); //this is index 0
+  path = "spawnBox.png";
+  setupTexture(editor.GUIOverhead, editor.editorTexture, path); //this is index 1
+  accessComplete();
   //continue here
 }
