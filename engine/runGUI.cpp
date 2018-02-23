0529f77daefa859af101824f9300e947714e7ef9
trashviolent <wizzardnoise@gmail.com>
2018-02-23 11:55:41 -0500

    Update runGUI.cpp

--- engine/runGUI.cpp
+++ engine/runGUI.cpp
@@ -39,4 +39,5 @@ void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) 
   //continue here
   //scenes have size attribute but it's not stored anywhere for startup.  store it in program.  scenePathAttributes is unfinished
   //now, and save will need fixing, and scene setup will need work and program.h will need stuff added to it
+  //setup sprites in relation to scene in sprite setup
 }
