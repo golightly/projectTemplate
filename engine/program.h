2bd62dba741f69a974098802af65661d3ba30a06
trashviolent <wizzardnoise@gmail.com>
2018-02-23 11:58:54 -0500

    Update program.h

--- engine/program.h
+++ engine/program.h
@@ -11,8 +11,6 @@ struct Program {
     std::string input, programPath, programName, programType, commandType, sceneImagePath;
     std::string* scenePath; //stores ALL scene paths
     std::string* sceneName; //stores ALL scene names
-    int* sceneWidth;
-    int* sceneHeight;
     int sceneNum, imageNum;
     Scene* scene;
     Image* image;
