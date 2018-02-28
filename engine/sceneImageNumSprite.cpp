adda1a08d9cf27cdfa9908740b868cf1db0f356c
trashviolent <wizzardnoise@gmail.com>
2018-02-28 16:22:23 -0500

    Update sceneImageNumSprite.cpp

--- engine/sceneImageNumSprite.cpp
+++ engine/sceneImageNumSprite.cpp
@@ -6,16 +6,19 @@
 #include "scene.h"
 #include "program.h"
 #include "sprite.h"
+#include "sceneImage.h"
 
 void sceneImageNumSprite(Program &program) {
   std::getline(program.readFile, program.input);
   program.scene.sceneImageNum = atoi(program.input.c_str());
-  program.scene.image = new int[program.scene.sceneImageNum];
+  program.scene.sceneImage = new SceneImage[program.scene.sceneImageNum];
   program.scene.spriteNum = 0;
   for(int a = 0; a < program.scene.sceneImageNum; ++a) {
     std::getline(program.readFile, program.input);
-    program.scene.image[a] = atoi(program.input.c_str());
-    program.scene.spriteNum += atoi(program.input.c_str()); //this needs correcting! probably.  how to know how many
-  }//sprites per image? image holds the index of the image required!  rework it all.
-  setupSprite(program); //wait, what image index.  there is no such thing... rewrite the stuff for the image variable
-}// to actually be the number of sprites per texture
+    program.scene.sceneImage[a].imagePath = atoi(program.input.c_str());
+    std::getline(program.readFile, program.input);
+    program.scene.sceneImage[a].numSprites = atoi(program.input.c_str());
+    program.scene.spriteNum += program.scene.sceneImage[a].numSprites;
+  }
+  setupSprite(program);
+}
