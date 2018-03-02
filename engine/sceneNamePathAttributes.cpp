52060900d2b73ac3f88de6488f628f3c83a9c51c
trashviolent <wizzardnoise@gmail.com>
2018-03-02 13:44:44 -0500

    Update sceneNamePathAttributes.cpp

--- engine/sceneNamePathAttributes.cpp
+++ engine/sceneNamePathAttributes.cpp
@@ -9,7 +9,7 @@
 void sceneNamePathAttributes(Program &program) {
   program.scene.sceneName = program.sceneName[atoi(program.sceneImagePath.c_str())];
   program.readFile.open(program.scenePath[atoi(program.sceneImagePath.c_str())].c_str());
-  std::getline(program.readFile, program.scene.functionName);
+  std::getline(program.readFile, program.scene.functionName); //will be NULL FUNCTION PATH the first time
   std::getline(program.readFile, program.input);
   program.scene.sceneAttributes.w = atoi(program.input.c_str());
   std::getline(program.readFile, program.inpit);
