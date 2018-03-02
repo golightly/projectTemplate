cabfcd62a94be6b4313c0f69309d2cb8e7ccda67
trashviolent <wizzardnoise@gmail.com>
2018-03-02 14:03:29 -0500

    Update sceneSetupCollision.cpp

--- engine/sceneSetupCollision.cpp
+++ engine/sceneSetupCollision.cpp
@@ -10,7 +10,8 @@
 void sceneSetupCollision(Program &program) {
   std:getline(program.readFile, program.input);
   program.scene.collisionNum = atoi(program.input.c_str());
-  program.scene.collisionAttributes = new SDL_Rect[program.scene.collisionNum];
+  if(program.scene.collisionNum > 0)
+    program.scene.collisionAttributes = new SDL_Rect[program.scene.collisionNum];
   for(int a = 0; a < program.scene.collisionNum; ++a) {
     std::getline(program.readFile, program.input);
     program.scene.collisionAttributes[a].w = atoi(program.input.c_str());
