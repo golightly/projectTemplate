c21c488da2e09a54bf7add689ad83e5a0bda9cf1
trashviolent <wizzardnoise@gmail.com>
2018-03-05 16:48:20 -0500

    Update runGUI.cpp

--- engine/runGUI.cpp
+++ engine/runGUI.cpp
@@ -1,27 +1,31 @@
 #include "runGUI.h"
 #include <string>
+#include <SDL.h>
 #include "editor.h"
 #include "scene.h"
 #include "overhead.h"
 #include "texture.h"
 #include "image.h"
 #include "access.h"
+#include "sceneImage.h"
+#include "render.h"
 
 void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) {
   accessData();
   setupOverhead(editor.GUIOverhead, editor.windowWidth, editor.windowHeight, windowName, editor);
   editor.texture.texture = NULL;
-  editor.editorTexture = NULL;
+  editor.editorTexture.texture = NULL;
   if(scene.sceneImageNum > 0) {
+    std::string* path = new std::string[scene.sceneImageNum];
     int* w = new int[scene.sceneImageNum];
     int* h = new int[scene.sceneImageNum];
-    std::string* path = new std::string[scene.sceneImageNum];
     for(int a = 0; a < scene.sceneImageNum; ++a) {
-      w[a] = (int)image[scene.image[a]].w;
-      h[a] = (int)image[scene.image[a]].h;
-      path[a] = image[scene.image[a]].path;
+      w[a] = (int)image[scene.sceneImage[a].imageIndex].w;
+      h[a] = (int)image[scene.sceneImage[a].imageIndex].h;
+      path[a] = image[scene.sceneImage[a].imageIndex].path;
     }
     setupTexture(editor.GUIOverhead, editor.texture, w, h, path, scene.sceneImageNum);
+    
   }
   delete [] w;
   delete [] h;
@@ -32,10 +36,24 @@ void runGUI(std::string windowName, Editor &editor, Scene &scene, Image* image) 
   w = 50;
   h = 50;
   path = "collisionBox.png";
-  setupTexture(editor.GUIOverhead, editor.editorTexture, path); //this is index 0
+  setupTexture(editor.GUIOverhead, editor.editorTexture, w, h, path); //this is index 0
   path = "spawnBox.png";
-  setupTexture(editor.GUIOverhead, editor.editorTexture, path); //this is index 1
+  setupTexture(editor.GUIOverhead, editor.editorTexture, w, h, path); //this is index 1
   accessComplete();
+  bool quit = false;
+  SDL_Event event;
+  while(!quit) {
+    while(SDL_PollEvent(&event) != 0) {
+      if(event.type == SDL_QUIT)
+        quit = true;
+    }
+    SDL_RenderClear(editor.GUIOverhead.renderer);
+    for(int a = 0; a < scene.spriteNum; ++a) {
+      render(editor.GUIOverhead, editor.texture[scene.sprite[a].textureIndex] //continue
+    }
+    //define sprites for editorTextures!
+  }
   //continue here
-  //setup sprites in relation to scene in sprite setup
+  //setup sprites in relation to scene in sprite setup - done
+  //error in how data is accessed, this doesn't actually protect it at all! restructure
 }
