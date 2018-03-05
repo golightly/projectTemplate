ece0e7b0365bf82b711228db357b002f0de58933
trashviolent <wizzardnoise@gmail.com>
2018-03-05 18:23:40 -0500

    Update render.cpp

--- engine/render.cpp
+++ engine/render.cpp
@@ -3,9 +3,12 @@
 #include "overhead.h"
 #include "texture.h"
 #include "sprite.h"
+#include "editor.h"
 
 //renders one sprite per function call
-void render(Overhead &overhead, Texture &texture, Sprite &sprite) {
-    SDL_Rect renderQuad = { sprite.attributes.x, sprite.attributes.y, sprite.attributes.w, sprite.attributes.h };
+void render(Editor &editor, Overhead &overhead, Texture &texture, Sprite &sprite) {
+    int x = sprite.attributes.x - editor.cameraX;
+    int y = sprite.attributes.y - editor.cameraY;
+    SDL_Rect renderQuad = { x, y, sprite.attributes.w, sprite.attributes.h };
     SDL_RenderCopy(overhead.renderer, texture.texture[sprite.textureIndex], &overhead.attributes, &renderQuad);
 }
