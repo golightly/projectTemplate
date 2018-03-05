9f76c7f364465bac621203aa8c4d10ed900d8b99
trashviolent <wizzardnoise@gmail.com>
2018-03-05 17:17:30 -0500

    Update access.h

--- engine/access.h
+++ engine/access.h
@@ -3,5 +3,6 @@
 #include <string>
 #include "editor.h"
 #include "sprite.h"
+#include "scene.h"
 
-void accessData(Editor &editor, std::string protectedType, std::string actionType, int index);
+void accessData(Editor &editor, Scene &scene, std::string protectedType, std::string actionType, int index);
