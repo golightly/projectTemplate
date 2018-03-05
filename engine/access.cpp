1494e6402bbb5f9d7214647742aa419928750341
trashviolent <wizzardnoise@gmail.com>
2018-03-05 17:49:02 -0500

    Update access.cpp

--- engine/access.cpp
+++ engine/access.cpp
@@ -1,11 +1,18 @@
 #include "access.h"
 #include <mutex>
+#include <string>
 #include "editor.h"
+#include "sprite.h"
+#include "render.h"
+#include "scene.h"
 
-void accessData(Editor &editor) {
-  editor.mu.lock();
-}
-
-void accessComplete(Editor &editor) {
-  editor.mu.unlock();
+//the only things that need protection right now are sprites, add more of these functions as required
+void accessData(Editor &editor, Scene &scene. std::string &protectedType, std::string &actionType, int &index) {
+  if(protectedType == "sprite") {
+    editor.spriteLock.lock();
+    if(actionType == "render") {
+      render(editor.GUIOverhead, editor.texture[scene.sprite[a].textureIndex], scene.sprite[index]);
+    }
+    editor.spriteLock.unlock();
+  }
 }
