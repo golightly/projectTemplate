6f884b940d0d8c563f17713b02f4e9f820f8b50d
trashviolent <wizzardnoise@gmail.com>
2018-03-06 14:49:50 -0500

    Update render.cpp

--- engine/render.cpp
+++ engine/render.cpp
@@ -10,5 +10,6 @@ void render(Editor &editor, Overhead &overhead, Texture &texture, Sprite &sprite
     int x = sprite.attributes.x - editor.cameraX;
     int y = sprite.attributes.y - editor.cameraY;
     SDL_Rect renderQuad = { x, y, sprite.attributes.w, sprite.attributes.h };
-    SDL_RenderCopy(overhead.renderer, texture.texture[sprite.textureIndex], &overhead.attributes, &renderQuad);
+    SDL_Rect sourceQuad = { 0, 0, texture.w[sprite.textureIndex], texture.h[sprite.textureIndex] };
+    SDL_RenderCopy(overhead.renderer, texture.texture[sprite.textureIndex], &sourceQuad, &renderQuad);
 }
