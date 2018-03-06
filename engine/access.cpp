3fa835bc7d59c3c603ea6e9946f66dff1d018a2a
trashviolent <wizzardnoise@gmail.com>
2018-03-05 20:02:34 -0500

    Update access.cpp

--- engine/access.cpp
+++ engine/access.cpp
@@ -10,10 +10,9 @@
 void accessData(Editor &editor, Scene &scene. std::string &protectedType, std::string &actionType, int &index) {
   if(protectedType == "sprite") {
     editor.spriteLock.lock();
-    if(actionType == "render") {
-      render(editor.GUIOverhead, editor.texture[scene.sprite[a].textureIndex], scene.sprite[index]);
-    }
-    else if(actionType = "checkPosition") {
+    if(actionType == "render")
+      render(editor.GUIOverhead, editor.texture[scene.sprite[index].textureIndex], scene.sprite[index]);
+    else if(actionType == "checkPosition") {
       actionType = "false";
       if((scene.sprite[index].attributes.x + scene.sprite[index].attributes.w) > editor.cameraX || scene.sprite[index].attributes.x < (editor.cameraX + editor.windowWidth)) {
         if((scene.sprite[index].attributes.y + scene.sprite[index].attributes.h) > editor.cameraY || scene.sprite[index].attributes.y < (editor.cameraY + editor.windowHeight))
@@ -21,5 +20,16 @@ void accessData(Editor &editor, Scene &scene. std::string &protectedType, std::s
       }
     }
     editor.spriteLock.unlock();
+  }
+  else if(protectedType == "editorSprite") {
+    editor.editorSpriteLock.lock();
+    if(actionType == "render")
+      render(editor.GUIOverhead, editor.editorTexture[editor.editorSprite[index].textureIndex, editor.editorSprite[index]);
+    else if(actionType == "checkPosition") {
+      actionType = false;
+      if((editor.editorSprite[index].attributes.x + editor.editorSprite[index].attributes.w) > editor.cameraX || editor.editorSprite[index].attributes.x < (editor.cameraX + editor.windowWidth)) {
+        if((editor.editorSprite[index].attributes.y + editor.editorSprite[index].attributes.h) > editor.cameraY || editor.editorSprite[index].attributes.y < (editor.cameraY + editor.windowHeight))
+          actionType = "true";
+    }
   }
 }
