87c7364656d7a65ea5e01f4a961e5848cd46dd02
trashviolent <wizzardnoise@gmail.com>
2018-03-06 13:53:52 -0500

    Update text.h

--- engine/text.h
+++ engine/text.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <SDL.h>
+#include <string>
+#include "sprite.h"
+
 struct Text {
-  
+  std::string name;
+  int fontIndex;
+  int* characterIndex; //the index of each character in the line of text, can range from space (0) to tilde (94)
+  SDL_Rect lineAttributes;
+  SDL_Rect* characterAttributes;
 };
+
+void setupText(Text &text, std::string name, int fontIndex, std::string line);
