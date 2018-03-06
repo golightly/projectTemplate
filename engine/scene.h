3591bf2c88f896e950195efe1e05edde3f3ec6ec
trashviolent <wizzardnoise@gmail.com>
2018-03-06 13:54:00 -0500

    Update scene.h

--- engine/scene.h
+++ engine/scene.h
@@ -5,12 +5,15 @@
 #include "program.h"
 #include "sprite.h"
 #include "sceneImage.h"
+#include "text.h"
 
 //will require a lot more
 struct Scene {
     std::string sceneName, functionName; //functionName matches the id used by the pointer for the function intended for this scene
     SDL_Rect sceneAttributes; //attributes of the scene itself
     int sceneImageNum; //number of images/texture required for the scene
+    int sceneFontNum; //number of fonts required for the scene
+    int sceneTextNum; //number of text objects (1 per line of text) required for the scene
     SceneImage* sceneImage; //contains number of sprites per texture and the index of that image
     int spriteNum;
     Sprite* sprite; //all sprites in the scene
@@ -20,6 +23,7 @@ struct Scene {
     int* spawnGroupSize; //spawn groups can have different sizes, respresents size of second dimension in spawnArea
     SDL_Rect** spawnArea; //spawn areas.  first dimension is spawn groups, then second dimension is specific attributes to spawn at
     //to be added, characters, and other stuff I'm sure
+    Text* text; //all lines of text in the scene
 };
 
 void setupScene(Program &program);
