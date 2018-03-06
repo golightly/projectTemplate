59c655c33a5f88b5a29650fcc72dba332c86d1e2
trashviolent <wizzardnoise@gmail.com>
2018-03-06 13:33:32 -0500

    Update saveProject.cpp

--- engine/saveProject.cpp
+++ engine/saveProject.cpp
@@ -20,6 +20,12 @@ void saveProject(Program &program) {
   for(int a = 0; a < program.sceneNum; ++a) {
     program.writeFile << program.scenePath[a] << "\n";
   }
+  program.writeFile << program.fontNum << "\n";
+  for(int a = 0; a < program.fontNum; ++a) {
+    for(int b = 0; b < 95; ++b) {
+      program.writeFile << program.font[a].character[b].path << "\n";
+    }
+  }
   program.writeFile.close();
   program.writeFile.clear();
 }
