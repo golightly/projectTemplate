e7f363a554c3eb55ce5b0bb284fa3124c3566e13
trashviolent <wizzardnoise@gmail.com>
2018-03-05 19:35:50 -0500

    Update openScene.cpp

--- engine/openScene.cpp
+++ engine/openScene.cpp
@@ -19,6 +19,56 @@ void openScene(Program &program) {
   //use editor to store all the sdl and editing scene related stuff
   //have editor also store the path to save to, for simplicity
   Editor editor;
+  editor.editorSpriteNum = 0;
+  std::ostringstream sstream;
+  editor.editorSpriteNum += scene.collisionNum;
+  if(editor.editorSpriteNum > 0)
+    Sprite* editorTemp = new Sprite[editor.editorSpriteNum];
+  for(int a = 0; a < editor.editorSpriteNum; ++a) {
+    editor.editorTemp[a].spriteName = "collisionBox";
+    sstream << a;
+    editor.editorTemp[a].spriteName += sstream.str();
+    sstream.str(std::string());
+    sstream.clear();
+    editor.editorTemp[a].textureIndex = 0;
+    editor.editorTemp[a].attributes.w = scene.collisionAttributes[a].w;
+    editor.editorTemp[a].attributes.h = scene.collisionAttributes[a].h;
+    editor.editorTemp[a].attributes.x = scene.collisionAttributes[a].x;
+    editor.editorTemp[a].attributes.y = scene.collisionAttributes[a].y;
+  }
+  for(int a = 0; a < scene.spawnGroupNum; ++a) {
+    editor.editorSpriteNum += scene.spawnGroupSize[a];
+  }
+  if(editor.editorSpriteNum > 0) {
+    editor.editorSprite = new Sprite[editor.editorSpriteNum];
+    if(scene.sceneCollisionNum > 0) {
+      for(int a = 0; a < scene.collisionNum; ++a) {
+        editor.editorSprite[a].spriteName = editorTemp[a].spriteName;
+        editor.editorSprite[a].textureIndex = editorTemp[a].textureIndex;
+        editor.editorSprite[a].attributes.w = editorTemp[a].attributes.w;
+        editor.editorSprite[a].attributes.h = editorTemp[a].attributes.h;
+        editor.editorSprite[a].attributes.x = editorTemp[a].attributes.x;
+        editor.editorSprite[a].attributes.y = editorTemp[a].attributes.y;
+      }
+      delete [] editorTemp;
+    }
+    int c = scene.collisionNum;
+    for(int a = 0; a < scene.spawnGroupNum; ++a) {
+      for(int b = 0; b < scene.spawnGroupSize[a]; ++b) {
+        editor.editorSprite[c].spriteName = "spawnBox";
+        sstream << (c - scene.collisionNum);
+        editor.editorSprite[c].spriteName += sstream.str();
+        sstream.str(std::string());
+        sstream.clear();
+        editor.editorSprite[c].textureIndex = 1;
+        editor.editorSprite[c].w = scene.spawnArea[a][b].w;
+        editor.editorSprite[c].h = scene.spawnArea[a][b].h;
+        editor.editorSprite[c].x = scene.spawnArea[a][b].x;
+        editor.editorSprite[c].y = scene.spawnArea[a][b].y;
+        ++c;
+      }
+    }
+  }
   std::cout << "width of editor window: ";
   std::cin >> editor.windowWidth;
   std::cout << "height of editor window: ";
