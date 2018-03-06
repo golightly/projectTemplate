67452993971378f58ebd6376b4fe3b2ea2f046c4
trashviolent <wizzardnoise@gmail.com>
2018-03-06 13:37:36 -0500

    Update exitSDK.cpp

--- engine/exitSDK.cpp
+++ engine/exitSDK.cpp
@@ -2,6 +2,7 @@
 #include "program.h"
 #include "scene.h"
 #include "image.h"
+#include "font.h"
 
 void exitSDK(Program &program, bool &quitSDK) {
     if(program.image != NULL)
@@ -10,6 +11,11 @@ void exitSDK(Program &program, bool &quitSDK) {
         delete [] program.scenePath;
     if(program.sceneName != NULL)
         delete [] program.sceneName;
+    for(int a = 0; a < program.fontNum; ++a) {
+        delete [] program.font[a].character;
+    }
+    delete [] program.font;
+    program.font = NULL;
     program.image = NULL;
     program.scenePath = NULL;
     program.sceneName = NULL;
