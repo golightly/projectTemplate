84c54c862f46186175e4f34cc26b10208b12f7b8
trashviolent <wizzardnoise@gmail.com>
2018-03-07 17:48:25 -0500

    Update font.h

--- engine/font.h
+++ engine/font.h
@@ -6,5 +6,5 @@
 struct Font { //restructure font as a set of character sets, each with 95 characters, filled in when the scene is initialised
   Charset* charset;
 };
-
-void setupFont(Font &font, std::string* path);
+//setup one at a time
+void setupFont(Font &font, std::string &path);
