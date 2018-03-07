7d8e9dbb0a3639a2452bf124e891b10fdb9c25ec
trashviolent <wizzardnoise@gmail.com>
2018-03-07 18:43:07 -0500

    Update font.cpp

--- engine/font.cpp
+++ engine/font.cpp
@@ -1,26 +1,53 @@
 #include "font.h"
 #include <string>
+#include <SDL.h>
+#include <SDL_ttf.h>
 #include <fstream>
 #include <cstdint>
 #include "image.h"
+#include "charset.h"
+#include "character.h"
 
-void setupFont(Font &font,int fontNum, std::string* path, std::string* fontName, int* charNum, int* charSize) {
+void setupFont(Font &font,int fontNum, std::string* path, std::string* fontName, int* charNum, int* charSize, SDL_Color* colour) {
   font.fontNum = fontNum;
   font.fontName = new std::string[font.fontNum];
+  font.charset = new Charset[font.fontNum];
   for(int a = 0; a < font.fontNum; ++a) {
     font.fontName[a] = fontName[a];
-  } //restart here
-  char* memblock;
-  std::ifstream readFile;
-  for(int a = 0; a < 95; ++a) {
-    font.character[a].path = path[a];
-    font.character[a].name += (char)(a+32); //a font should be every ascii character from space (32) to tilde (126)
-    readFile.open(font.character[a].path.c_str(), std::ios::in | std::ios::binary | std::ios::beg);
-    memblock = new char[sizeof(uint16_t))];
-    readFile.read(memblock, sizeof(uint16_t));
-    font.character[a].w = (uint16_t)memblock;
+    font.charset[a].charNum = charNum[a];
+    font.charset[a].charSize = charSize[a];
+    font.charset[a].character = new Character[font.charset[a].charNum];
+    TTF_Font* newFont;
+    newFont = TTF_OpenFont(path[a].c_str(), font.charset[a].charSize);
+    for(int b = 0; b < font.charset[a].charNum; ++b) { //sdl_ttf is already initialised, in overhead setup
+      SDL_Surface* surface;
+      char letter[1];
+      letter[0] = b + 32;
+      surface = TTF_RenderText_Blended(newFont, letter, colour[a]);
+      font.charset[a].character[b].width = surface->w;
+      font.charset[a].character[b].height = surface->h;
+      font.charset[a].character[b].pixels = new float[(surface->w * surface->h) * 4];
+      int count = 0;
+      Uint8 red, green, blue, alpha;
+      SDL_LockSurface(surface);
+      Uint8* p = (Uint8*)surface->pixels;
+      for(int y = 0; y < surface->h; ++y) {
+        for(int x = 0; x < surface->w; ++x) {
+          Uint32 pixel = p[(y * surface->w) + x];
+          SDL_GetRGBA(pixel, surface->format, &red, &green, &blue, &alpha);
+          font.charset[a].character[b].pixels[count] = ((float)red / 255.0f);
+          ++count;
+          font.charset[a].character[b].pixels[count] = ((float)green / 255.0f);
+          ++count;
+          font.charset[a].character[b].pixels[count] = ((float)blue / 255.0f);
+          ++count;
+          font.charset[a].character[b].pixels[count] = ((float)alpha / 255.0f);
+          ++count;
+        }
+      }
+      SDL_UnlockSurface(surface);
+      SDL_FreeSurface(surface);
+      TTF_CloseFont(newFont);
+    }
   }
-  delete [] memblock;
-  readFile.close();
-  readFile.clear();
 }
