1453ea0fca1f3122a13ce0355d7cb3771e82a2c0
trashviolent <wizzardnoise@gmail.com>
2018-03-07 18:25:50 -0500

    Update font.h

--- engine/font.h
+++ engine/font.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <string>
+#include <SDL.h>
+#include <SDL_ttf.h>
 #include "charset.h"
 
 struct Font { //restructure font as a set of character sets, each with 95 characters, filled in when the scene is initialised
@@ -9,7 +11,7 @@ struct Font { //restructure font as a set of character sets, each with 95 charac
   int fontNum;
 };
 //setup all fonts at once
-void setupFont(Font &font, int fontNum,  std::string* path, std::string fontName, int* charNum, int* charSize);
+void setupFont(Font &font, int fontNum,  std::string* path, std::string fontName, int* charNum, int* charSize, SDL_Color* colour);
 
 //move font to scene!
 
