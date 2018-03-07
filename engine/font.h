fa5327a4557efa3bcc129b3ec560ae29b9c5d37e
trashviolent <wizzardnoise@gmail.com>
2018-03-07 18:49:22 -0500

    Update font.h

--- engine/font.h
+++ engine/font.h
@@ -13,17 +13,7 @@ struct Font { //restructure font as a set of character sets, each with 95 charac
 //setup all fonts at once
 void setupFont(Font &font, int fontNum,  std::string* path, std::string fontName, int* charNum, int* charSize, SDL_Color* colour);
 
+void closeFont();
+
 //move font to scene!
 
-//Font font;
-
-//something like this
-
-/*std::getline(program.readFile, program.input);
-        program.fontNum = atoi(program.input.c_str());
-        program.fontPath = new std::string[program.fontNum];
-        program.fontName = new std::string[program.fontNum];
-        for(int a = 0; a < program.fontNum; ++a) {
-            std::getline(program.readFile, program.fontPath[a]);
-            std::getline(program.readFile, program.fontName[a]);
-        }/*
