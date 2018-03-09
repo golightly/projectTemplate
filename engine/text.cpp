cae6218cb5fcebe8f324545f60e9e9665532a794
trashviolent <wizzardnoise@gmail.com>
2018-03-09 15:06:29 -0500

    Update text.cpp

--- engine/text.cpp
+++ engine/text.cpp
@@ -3,9 +3,9 @@
 void setupText(Text &text, Font &font, std::string name, int fontIndex, std::string line) {
   text.name = name;
   text.fontIndex = fontIndex;
-  characterIndex = new int[line.size()];
+  characterIndex = new char[line.size()];
   for(int a = 0; a < line.size(); ++a) {
-    characterIndex[a] = (int)(line[a] - 32);
+    characterIndex[a] = line[a] - 32;
   }
   //decide on a base size for all characters, they are not necessarily all the same size, gonna need to do that
   //at home or otherwise with an art program
