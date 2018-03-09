95c0c1c6f05c4af251972a8203925877b10f93f9
trashviolent <wizzardnoise@gmail.com>
2018-03-09 13:46:57 -0500

    Update main.cpp

--- fontProject/main.cpp
+++ fontProject/main.cpp
@@ -26,14 +26,10 @@ int main(int argc, char* args[]) {
 	SDL_Color color = { 255, 0, 0 };
 	surface = TTF_RenderText_Solid(font, "testing", color);
 	SDL_SetSurfaceAlphaMod(surface, 255);
-	//std::cout << (int)surface->format->BytesPerPixel << std::endl;
-	//std::cout << (int)surface->format->BitsPerPixel << std::endl;
-	//char input;
-	//std::cin >> input;
 	int surfaceWidth = surface->w;
 	int surfaceHeight = surface->h;
 	Uint8 red, green, blue, alpha;
-	float* textImage = new float[(surfaceWidth * surfaceHeight) * 4];
+	unsigned char* textImage = new unsigned char[(surfaceWidth * surfaceHeight) * 4];
 	int countText = 0;
 	SDL_LockSurface(surface);
 	Uint8* p = (Uint8*)surface->pixels;
@@ -41,20 +37,20 @@ int main(int argc, char* args[]) {
 		for (int x = 0; x < (surfaceWidth); ++x) {
 			Uint8 pixel = p[y * surface->pitch + x];
 			SDL_GetRGBA(pixel, surface->format, &red, &green, &blue, &alpha);
-			textImage[countText] = ((float)red / 255.0f);
+			textImage[countText] = red;
 			++countText;
-			textImage[countText] = ((float)green / 255.0f);
+			textImage[countText] = green;
 			++countText;
-			textImage[countText] = ((float)blue / 255.0f);
+			textImage[countText] = blue;
 			++countText;
-			textImage[countText] = ((float)alpha / 255.0f);
+			textImage[countText] = alpha;
 			++countText;
 		}
 	}
 	SDL_UnlockSurface(surface);
 	SDL_FreeSurface(surface);
 	GLuint texture;
-	float* image;
+	unsigned char* image;
 	int width = 1000, height = 1000;
 	int textX = width - (int)(width / 1.5);
 	int textY = height - (int)(height / 1.5);
