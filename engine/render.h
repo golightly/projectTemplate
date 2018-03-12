011db1df555c373e4050a96003987eb19016b852
trashviolent <wizzardnoise@gmail.com>
2018-03-12 15:14:36 -0400

    Update render.h

--- engine/render.h
+++ engine/render.h
@@ -1,5 +1,8 @@
 #include "overhead.h"
 #include "texture.h"
 #include "sprite.h"
+#include "editor.h"
+
+void render(Editor &editor, Overhead &overhead, Texture &texture, Sprite &sprite);
 
 void render(Overhead &overhead, Texture &texture, Sprite &sprite);
