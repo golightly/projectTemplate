85338c17dc4ff34132c9476c0fa8d4438d2dea5a
golightly <wizzardnoise@gmail.com>
2018-05-25 14:01:00 -0400

    Update writeFile.py

--- image prep/writeFile.py
+++ image prep/writeFile.py
@@ -1,8 +1,9 @@
 import subprocess
 from PIL import Image
-path = raw_input("path: ")
-output = raw_input("output path: ")
-file = Image.open(path)
+path = raw_input("file name: ")
+inputPath = path + ".png"
+outputPath = path + ".img"
+file = Image.open(inputPath)
 w, h = file.size
 pixels = file.convert("RGBA")
 pixelArray = [[0 for y in range(w)] for x in range(h)]
@@ -10,9 +11,9 @@ for one in range(h):
 	for two in range(w):
 		r,g,b,a = pixels.getpixel((two,one))
 		pixelArray[one][two] = str(r) + " " + str(g) + " " + str(b) + " " + str(a) + " "
-file2 = open(output,"w")
+file2 = open(outputPath,"w")
 for a in range(h):
 	for b in range(w):
 		file2.write(pixelArray[a][b])
 file2.close()
-subprocess.check_call(["editFile.exe", str(output), str(w), str(h)])
+subprocess.check_call(["editFile.exe", str(outputPath), str(w), str(h)])
